a69b437864ce0b09c3326be86780c3ccd880603f
Hazar Gul Nazari <nazariazargul@gmail.com>
2023-01-25 22:27:50 +0100

    add problem details example

--- src/main/java/com/kn/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/kn/demo/exceptions/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.kn.demo.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
