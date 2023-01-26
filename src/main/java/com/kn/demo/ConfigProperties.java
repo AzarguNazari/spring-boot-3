3b40aeb1948fa5352c72ab3d33ce919338e81947
hazar.nazari <hazar.nazari@kuehne-nagel.com>
2023-01-26 14:42:11 +0100

    commit

--- src/main/java/com/kn/demo/ConfigProperties.java
+++
@@ -1,7 +0,0 @@
-package com.kn.demo;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
-
-@ConfigurationProperties(prefix = "config")
-public record ConfigProperties(String prop1, String prop2) {}
--- src/main/java/com/kn/demo/MyApp.java
+++ src/main/java/com/kn/demo/MyApp.java
@@ -1,45 +1,14 @@
 package com.kn.demo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ConfigProperties.class)
 public class MyApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApp.class, args);
 	}
 
-
-	@Bean
-	ApplicationRunner appStarted(ConfigProperties configProperties) {
-		return args -> {
-			System.out.println(configProperties);
-//			var john = new Person(1L, "John");
-//			personRepository.save(john);
-//			personRepository.findAll().forEach(System.out::println);
-		};
-	}
-
 }
-
-//@Entity
-//record Person(@Id Long id, String name){
-//	Person(){
-//		this(0L, null);
-//	}
-//}
-//
-//interface PersonRepository extends JpaRepository<Person, Long> {}
-
-
